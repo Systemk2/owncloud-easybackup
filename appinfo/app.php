974d67f9481a718473c474175519d23d5f7df111
Systemk2 <s.kanzow@systemk2.de>
2014-12-13 21:25:25 +0100

    App's entry URL now correctly pointing to /

--- appinfo/app.php
+++ appinfo/app.php
@@ -26,23 +26,23 @@
 
 \OCP\App::addNavigationEntry(
 		array (
-				
+
 				// the string under which your app will be referenced in owncloud
 				'id' => 'easybackup',
-				
+
 				// sorting weight for the navigation. The higher the number, the higher
 				// will it be listed in the navigation
 				'order' => 10,
-				
+
 				// the route that will be shown on startup
-				'href' => \OCP\Util::linkToRoute('easybackup.page.configuration'),
-				
+				'href' => \OCP\Util::linkToRoute('easybackup.page.index'),
+
 				// the icon that will be shown in the navigation
 				// this file needs to exist in img/...
 				'icon' => \OCP\Util::imagePath('easybackup', 'easybackup_logo.png'),
-				
+
 				// the title of your application. This will be used in the
 				// navigation or on the settings page of your app
-				'name' => 'EasyBackup' 
+				'name' => 'EasyBackup'
 		));
 
